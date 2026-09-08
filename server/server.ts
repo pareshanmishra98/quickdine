dd2c80d8c1fe70442afd0125dd154ff51ac70608
pareshanmishra98 <prashant908mishra@gmail.com>
2026-09-08 10:00:25 +0530

    created API for authentication

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 const app = express();
 
 // connect to mongoDB
@@ -17,6 +18,17 @@ const port = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use("/api/auth", authRouter)
+
+//global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error("Unhandle Error:", err);
+    res.status(500).json({
+        message: err.message || "Internal Server Error",
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
